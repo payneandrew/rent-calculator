14cf35371a882eebc1fa69665a4ba26edc7bfca9
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2023-04-05 19:01:49 -0400

    Working checkboxes

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,77 +1,28 @@
 import React, { useState } from "react";
 
-function CheckboxComponent() {
-  const [checkboxValues, setCheckboxValues] = useState({
-    checkbox1: false,
-    checkbox2: false,
-    checkbox3: false,
-    checkbox4: false,
-    checkbox5: false,
-  });
+interface CheckboxProps {
+  label: string;
+  isChecked: boolean;
+  onChange: (isChecked: boolean) => void;
+}
 
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    setCheckboxValues({
-      ...checkboxValues,
-      [name]: checked,
-    });
-  };
+export default function Checkbox(props: CheckboxProps) {
+  function handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (props.onChange) {
+      props.onChange(event.target.checked);
+    }
+  }
 
   return (
-    <div className="flex flex-col space-y-4">
-      <h1 className="font-bold">Room Size</h1>
-      <label className="flex items-center space-x-2">
+    <div>
+      <label>
         <input
           type="checkbox"
-          name="checkbox1"
-          checked={checkboxValues.checkbox1}
+          checked={props.isChecked}
           onChange={handleCheckboxChange}
-          className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
         />
-        <span>Tiny</span>
-      </label>
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          name="checkbox2"
-          checked={checkboxValues.checkbox2}
-          onChange={handleCheckboxChange}
-          className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
-        />
-        <span>Small</span>
-      </label>
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          name="checkbox3"
-          checked={checkboxValues.checkbox3}
-          onChange={handleCheckboxChange}
-          className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
-        />
-        <span>Medium</span>
-      </label>
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          name="checkbox4"
-          checked={checkboxValues.checkbox4}
-          onChange={handleCheckboxChange}
-          className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
-        />
-        <span>Large</span>
-      </label>
-      <label className="flex items-center space-x-2">
-        <input
-          type="checkbox"
-          name="checkbox5"
-          checked={checkboxValues.checkbox5}
-          onChange={handleCheckboxChange}
-          className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
-        />
-        <span>Extra Large</span>
+        {props.label}
       </label>
     </div>
   );
 }
-
-export default CheckboxComponent;
--- components/RoomCard.tsx
+++ components/RoomCard.tsx
@@ -1,8 +1,20 @@
-import Image from "next/image";
-import CheckboxComponent from "./CheckBox";
+import Checkbox from "./Checkbox";
 import Input from "./Input";
+import { useState } from "react";
 
-export default function RoomCard() {
+export default function RoomCard(props) {
+  const [checkboxValues, setCheckboxValues] = useState({
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+    checkbox4: false,
+    checkbox5: false,
+  });
+  const [selectedOption, setSelectedOption] = useState(null);
+  function handleCheckboxChange(index, isChecked) {
+    setSelectedOption(isChecked ? index : null);
+  }
+
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden m-4 md:w-1/2 lg:w-1/3">
       <div className="p-6">
@@ -14,7 +26,44 @@ export default function RoomCard() {
             </label>
           </li>
           <li>
-            <CheckboxComponent />
+            <div className="flex flex-col space-y-4">
+              <h1 className="font-bold">Room Size</h1>
+              <label className="flex items-center space-x-2">
+                <Checkbox
+                  isChecked={selectedOption === 1}
+                  onChange={(isChecked) => handleCheckboxChange(1, isChecked)}
+                />
+                <span>Tiny</span>
+              </label>
+              <label className="flex items-center space-x-2">
+                <Checkbox
+                  isChecked={selectedOption === 2}
+                  onChange={(isChecked) => handleCheckboxChange(2, isChecked)}
+                />
+                <span>Small</span>
+              </label>
+              <label className="flex items-center space-x-2">
+                <Checkbox
+                  isChecked={selectedOption === 3}
+                  onChange={(isChecked) => handleCheckboxChange(3, isChecked)}
+                />
+                <span>Medium</span>
+              </label>
+              <label className="flex items-center space-x-2">
+                <Checkbox
+                  isChecked={selectedOption === 4}
+                  onChange={(isChecked) => handleCheckboxChange(4, isChecked)}
+                />
+                <span>Large</span>
+              </label>
+              <label className="flex items-center space-x-2">
+                <Checkbox
+                  isChecked={selectedOption === 5}
+                  onChange={(isChecked) => handleCheckboxChange(5, isChecked)}
+                />
+                <span>Extra Large</span>
+              </label>
+            </div>
           </li>
         </ul>
       </div>
