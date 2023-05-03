0f20a8049365fa0a93b47f7537d40b532394d51d
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2023-05-03 15:30:04 -0400

    clean up

--- utils/helpers/rentCalculator.ts
+++ utils/helpers/rentCalculator.ts
@@ -3,7 +3,7 @@ import { RoomsProps } from "@/types/rooms";
 export const calculateTotalSquareFootage = (rooms: RoomsProps[]) => {
   return rooms
     .map((room) => Number(room.roomSize))
-    .reduce((a, b) => {
-      return a + b;
+    .reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
     }, 0);
 };
